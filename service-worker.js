ed68c3a8ae0e4971438d580ed2413015e6750c6b
tas755 <65523662+ts2020-bot@users.noreply.github.com>
2025-10-14 23:04:39 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,15 @@
-const CACHE_NAME = "weather-okayama-v1";
-const urlsToCache = [
-  "index.html",
-  "manifest.json",
-  "icon-192.png",
-  "icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("weather-cache").then(cache => {
+      return cache.addAll(["./", "./index.html", "./manifest.json"]);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
     })
   );
 });
